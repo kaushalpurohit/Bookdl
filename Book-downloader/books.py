38c48c3e0a52552d77f04dcc00ddb8d3e6b250b6
kaushalpurohit <kpurohit43@gmail.com>
2020-10-01 17:20:43 +0530

    Add README changes

---
+++ Book-downloader/books.py
@@ -0,0 +1,36 @@
+''' Class to hold book data. '''
+
+from termcolor import colored
+
+class books:
+    def __init__(self):
+        self.dict = dict()
+        self.itr = 1
+
+    def add(self, index, title, link):
+        self.dict[index] = {}
+        self.dict[index]['Title'] = title
+        self.dict[index]['Link'] = link
+
+    def get_results(self):
+        return self.dict
+
+    def show_results(self):
+        size = len(self.dict)
+        last = self.itr + 5
+        i = self.itr
+        while i < last:
+            try:
+                title = self.dict[i]['Title']
+                print("{}.{}".format(colored(f"[{i}]",'magenta', attrs = ['bold']),colored(title,'yellow', attrs = ['bold'])))
+            except:
+                break
+            i += 1
+        if(i <= size): print("{}.{}".format(colored("[0]", "magenta", attrs = ['bold']), colored("more","yellow", attrs = ['bold'])))
+        self.itr = i
+
+    def get_url(self,i):
+        return self.dict[i]['Link']
+
+    def get_title(self,i):
+        return self.dict[i]['Title']
---
+++ Book-downloader/main.py
@@ -0,0 +1,32 @@
+import argparse
+import sys
+from logger import logger
+from pdfdrive import search, download
+from books import books
+
+logger = logger()
+parser = argparse.ArgumentParser(description = 'A Program to download books from pdf-drive.')
+
+parser.add_argument("Book_name", type = str, help = "Book name to search for")
+args = parser.parse_args()
+
+def main():
+    obj = books()
+    search(args.Book_name,obj)
+    response = 0
+    while(True):
+        obj.show_results()
+        response = input("Enter choice:")
+        logger.debug(response)
+        if(response != '0'):
+            break
+    url = obj.get_url(int(response))
+    title = obj.get_title(int(response))
+    download(title,url)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        logger.info("\nExiting...")
+        sys.exit()
