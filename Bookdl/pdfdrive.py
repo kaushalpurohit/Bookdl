2a93bda672aceaa799ce3ace655a10a037de9bde
Krithic Kumar <krithickumarub@gmail.com>
2021-01-27 00:29:06 +0530

    a more elegant way to download books

--- Bookdl/pdfdrive.py
+++ Bookdl/pdfdrive.py
@@ -1,13 +1,12 @@
 ''' Contains functions to search and download the book '''
 
-from selenium import webdriver
-from bs4 import BeautifulSoup
-from . import books, save
-from termcolor import colored
-from . import logger
 import sys
-import requests
 import re
+import requests
+from bs4 import BeautifulSoup
+from termcolor import colored
+from . import books, save
+from . import logger
 
 logger = logger.logger()
 
@@ -20,44 +19,29 @@ def search(book_name, book):
 
     print("searching for " + colored(book_name, "green", attrs=['bold']))
     url = "https://www.pdfdrive.com/search?q={}".format(book_name)
+
     source = requests.get(url)
     soup = BeautifulSoup(source.content, 'html5lib')
     results = soup.findAll('a', attrs={'class': 'ai-search'})
-    i = 1
-    for result in results:
+
+    for i, result in enumerate(results):
         title = result.find('h2').text
         link = result['href']
         book.add(i, title, link)
-        i += 1
 
 
-def download(title, url):
+def download(title, url, ext):
     '''Using selenium driver here to get the download link.'''
 
     url = "https://www.pdfdrive.com" + url
-    source = requests.get(url)
-    soup = BeautifulSoup(source.content, 'html5lib')
-    results = soup.find('a', attrs={'id': 'download-button-link'})
-    link = results['href']
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
-    driver_path = "/usr/lib/chromium/chromedriver"
-    driver = webdriver.Chrome(driver_path, options=chrome_options)
-    driver.get("https://www.pdfdrive.com" + link)
-    driver.set_page_load_timeout(10)
-    driver.set_script_timeout(10)
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html5lib')
-    button = soup.find('a', attrs={'onclick': re.compile('AiD(.*)')})
-    if button is not None:
-        try:
-            link = re.findall('http(.*)', button['href'])
-            pdf = "http" + link[0]
-        except Exception as e:
-            logger.debug(e)
-            pdf = "https://www.pdfdrive.com" + button['href']
-        save.save(title, pdf)
-    else:
-        print("Book not found!")
-        print("Exiting...")
-        sys.exit()
+    resp = requests.get(url)
+    soup = BeautifulSoup(resp.content, 'html5lib')
+
+    bookId = soup.find('button', attrs={'id': 'previewButtonMain'})['data-id']
+    session = re.findall(r'session=(.+?)"', str(soup))[0]
+    logger.debug(bookId)
+    logger.debug(session)
+
+    url = "https://www.pdfdrive.com/download.pdf"
+    parameters = {'id': bookId, 'h': session, 'ext': ext}
+    save.save(title, url, parameters)
--- Bookdl/save.py
+++ Bookdl/save.py
@@ -2,47 +2,47 @@ import requests
 import re
 import sys
 import os
+import time
 from . import logger
 from termcolor import colored
-import time
 
 logger = logger.logger()
 
 
-def save(title, url):
+def save(title, url, parameters):
     '''Function to save the file in downloads directory.'''
 
     try:
-        extension = re.findall('.*ext=(.*)', url)
-        extension = '.' + extension[0].strip(' ')
+        extension = '.' + parameters['ext']
     except Exception as e:
         logger.debug(e)
         extension = ".pdf"
 
     home = os.path.expanduser("~")
     logger.debug(home)
-    Books = os.path.join(home, "Books/")
-    if not os.path.exists(Books):
-        os.makedirs(Books)
-    path = Books + title + extension
-    if check(path):
-        with open(path, 'wb') as f:
-            try:
-                response = requests.get(url, stream=True)
-            except Exception as e:
-                logger.debug(e)
-                print("file not found!")
-                sys.exit()
+    bookPath = os.path.join(home, "Books/")
+    if not os.path.exists(bookPath):
+        os.makedirs(bookPath)
 
-            total = response.headers.get('content-length')
-            size = int(total) / (1024 * 1024)
-            size = round(size, 2)
-            if total is None:
-                f.write(response.content)
-            else:
-                progress_bar(title, total, path, response, f)
-    else:
+    path = bookPath + title + extension
+    if not check(path):
         print("No changes were made.")
+
+    with open(path, 'wb') as f:
+        try:
+            response = requests.get(url, params=parameters, stream=True)
+        except Exception as e:
+            logger.debug(e)
+            print("File not found!")
+            sys.exit()
+
+        total = response.headers.get('Content-Length')
+        size = int(total) / (1024 * 1024)
+        size = round(size, 2)
+        if total is None:
+            f.write(response.content)
+        else:
+            progress_bar(title, total, path, response, f)
 
 
 def progress_bar(title, total, path, response, f):
@@ -50,23 +50,27 @@ def progress_bar(title, total, path, response, f):
 
     print(f"Downloading {colored(title, 'green', attrs = ['bold'])}")
     print(f"saving the file to: {path}")
+
     logger.debug(path)
     downloaded = 0
     total = int(total)
     chunk = max(int(total / 1000), 1024 * 1024)
     start = time.time()
+
     for data in response.iter_content(chunk_size=chunk):
         downloaded += len(data)
         downloaded_size = downloaded / (1024 * 1024)
         downloaded_size = round(downloaded_size, 2)
         speed = downloaded_size / (time.time() - start)
         eta = int((total - downloaded) / (speed * 1024 * 1024))
+
         if(speed > 1):
             speed = int(speed)
             speed = str(speed) + ' m'
         else:
             speed = int(speed * 1024)
             speed = str(speed) + ' k'
+
         done = int(50 * downloaded / total)
         details = "{} mb | {}bps |".format(int(downloaded_size), speed)
         completed = '▇' * done
@@ -74,8 +78,9 @@ def progress_bar(title, total, path, response, f):
         bar = " |{}{}|".format(completed, remaining)
         bar = colored(bar, "cyan")
         percent = done * 2
-        f.write(data)
         progress = "\r {} {}s {}{}%".format(details, eta, bar, percent)
+
+        f.write(data)
         sys.stdout.write(progress)
         sys.stdout.flush()
 
@@ -84,11 +89,13 @@ def progress_bar(title, total, path, response, f):
 
 
 def check(path):
+    '''Checks if the given path exists'''
+
     if os.path.exists(path):
         warning = "File already exists! Do you want to overwrite it?(y/n)"
         logger.warning(warning)
         choice = input()
         logger.debug(choice)
-        return True if choice == 'y' else False
+        return choice == 'y'
     else:
         return True
